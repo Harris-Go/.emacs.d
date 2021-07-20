63cd5fb531a88cc14e22f98d773bf3843ea34e42
Harris-Go <harrisgoodman1@gmail.com>
2021-07-20 19:04:19 +0100

    Update init.el
    Added use-package integration

--- init.el
+++ init.el
@@ -5,3 +5,7 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
